c0c699e5ffb891093229981f1bf8bf08dae18ee0
WeijieFu <20438157+WeijieFu@users.noreply.github.com>
2022-11-20 00:18:38 +0000

    add git ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b904e6078049fd9ae29a"; }
+/******/ 		__webpack_require__.h = function() { return "300dd794119e6896854f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
